84325f7ed6e880a04b1458ff2c26f82ca5d69fa3
ewelina-borucka <ewelina.borucka@gmail.com>
2017-01-27 00:44:50 +1100

    add functions for emoji

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,50 @@
 // -------------------------------------------------- //
+// Board generate
+var size = 8;
+
+$(function() { //on load
+    printBoard(size);
+    printBoard(size);
+    printBoard(size);
+    printBoard(size);
+});
+
+function printBoard(size) {
+    var maxRow = parseInt(size);
+    var maxCol = parseInt(size);
+
+    var myTable = $("<table></table>").appendTo("#board");
+    for (var row = maxRow - 1; row >= 0; row--) {
+        var myRow = $("<tr></tr>").appendTo(myTable);
+        for (var col = 0; col < maxCol; col++) {
+            myRow.append("<td></td>");
+        }
+    }
+}
+
+// -------------------------------------------------- //
 // Initialize socket connection
 
 var socket = io.connect( "http://localhost:8888" );
 
 
 $(function() {
-	var btnLeft = $( "#left" ),
-			btnRight = $( "#right" ),
-			btnSlow = $( "#slow" ),
-			btnStop = $( "#stop" ),
-			userIput = $( "#text" ),
-			btnText = $( "#btn-text" );
+	var btnLeft = $("#left"),
+			btnRight = $("#right"),
+			btnSlow = $("#slow"),
+			btnYo = $("#yo"),
+			btnStop = $("#stop"),
+			userIput = $("#text"),
+			btnText = $("#btn-text"),
+
+			btnSmile = $("#smile"),
+			btnBall = $("#ball"),
+			btnTongueout = $("#tongueout"),
+			btnWinkright = $("#winkright"),
+			btnSpeaker = $("#speaker"),
+			btnDonut = $("#donut"),
+			btnAngryface = $("#angryface"),
+			btnBlink = $("#blink");
 
 
 // -------------------------------------------------- //
@@ -36,11 +70,57 @@ $(function() {
 		socket.emit( "go", "stop" );
 	};
 
+	var yo = function() {
+		console.log( "user input yo" );
+		socket.emit( "userInput", "yo!!" );
+	};
+
+	var smile = function () {
+		socket.emit( "ledArt", emoji.smile );
+	};
+
+	var ball = function () {
+		socket.emit( "ledArt", emoji.ball );
+	};
+
+	var tongueout = function () {
+		socket.emit( "ledArt", emoji.tongueout );
+	};
+
+	var winkright = function () {
+		socket.emit( "ledArt", emoji.winkright );
+	};
+
+	var speaker = function () {
+		socket.emit( "ledArt", emoji.speaker );
+	};
+
+	var donut = function () {
+		socket.emit( "ledArt", emoji.donut );
+	};
+
+	var angryface = function () {
+		socket.emit( "ledArt", emoji.angryface );
+	};
+
+	var blink = function () {
+		socket.emit( "ledArt", emoji.blink );
+	};
+
 	btnLeft.on( "click", left );
 	btnRight.on( "click", right );
 	btnSlow.on( "click", slow );
 	btnStop.on( "click", stop );
+	btnYo.on( "click", yo);
 
+	btnSmile.on("click", smile);
+	btnBall.on("click", ball);
+	btnTongueout.on("click", tongueout);
+	btnWinkright.on("click", winkright);
+	btnSpeaker.on("click", speaker);
+	btnDonut.on("click", donut);
+	btnAngryface.on("click", angryface);
+	btnBlink.on("click", blink);
 
 	// -------------------------------------------------- //
 	// User input text area
@@ -50,6 +130,11 @@ $(function() {
 		console.log( text );
 		socket.emit( "userInput", text );
 	} );
+
+	// -------------------------------------------------- //
+	// User input buttons emoji
+
+
 
 
 } );
